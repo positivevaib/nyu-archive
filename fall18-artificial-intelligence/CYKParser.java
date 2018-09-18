12a225c9078e94628fedbe1398641038ec982163
positivevaib <vaib.gadodia@gmail.com>
2018-09-18 18:50:40 -0400

    First Command Line Commit

--- fall18-artificial-intelligence/CYKParser.java
+++ fall18-artificial-intelligence/CYKParser.java
@@ -1,29 +1,41 @@
 package artificial_intelligence_fall18;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CYKParser {
 	String[] sentence;
 	ArrayList<NonTerminal> grammar;
-	ArrayList<ArrayList<ArrayList<Tree>>> chart;
+	Tree[][][] chart;
 	
 	public CYKParser(String sentence, ArrayList<NonTerminal> grammar) {
 		this.sentence = sentence.split(" ");
 		this.grammar = grammar;
+		this.chart = new Tree[NonTerm.values().length][sentence.split(" ").length][sentence.split(" ").length];
 	}
 	
-	public ArrayList<ArrayList<ArrayList<Tree>>> parse() {
-		chart = new ArrayList<>();
-		
+	public Tree[][][] parse() {
 		int N = sentence.length;
 		for (int i = 0; i < N; i++) {
 			String word = sentence[i];
 			for (NonTerminal phrase : grammar) {
 				if (phrase.word.equals(word))
-					
+					this.chart[phrase.symbol.i][i][i] = new Tree(phrase.symbol, i, i, word, null, null, phrase.prob);
 			}
 		}
 		
-		return chart;
+		for (int len = 1; len < N; len++) {
+			for (int i = 0; i < (N + 1 - len); i++) {
+				int j = i + len - 1;
+				for (NonTerm M : NonTerm.values()) {
+					this.chart[M.ordinal()][i][j] = new Tree(M, i, j, null, null, null, 0.0);
+				}
+				for (int k = i; k < (j - 1); k++) {
+					
+				}
+			}
+		}
+		
+		return this.chart;
 	}
 }
--- fall18-artificial-intelligence/Tree.java
+++ fall18-artificial-intelligence/Tree.java
@@ -1,14 +1,14 @@
 package artificial_intelligence_fall18;
 
 public class Tree {
-	NonTerminal phrase;
+	NonTerm phrase;
 	int startPhrase, endPhrase;
 	String word;
 	Tree left;
 	Tree right;
 	double prob;
 	
-	public Tree(NonTerminal phrase, int startPhrase, int endPhrase, String word, Tree left, Tree right, double prob) {
+	public Tree(NonTerm phrase, int startPhrase, int endPhrase, String word, Tree left, Tree right, double prob) {
 		this.phrase = phrase;
 		this.startPhrase = startPhrase;
 		this.endPhrase = endPhrase;
