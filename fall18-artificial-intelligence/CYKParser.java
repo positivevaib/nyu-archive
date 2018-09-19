55ba2d77f7934f6f6017a3c22f96dd1862e38175
positivevaib <vaib.gadodia@gmail.com>
2018-09-18 21:06:42 -0400

    Update CYKParser.java, CallingClass.java and delete NonTerminal.java
    - Completed parse function in CYKParser.java
    - Added main function body and created print functions in CallingClass.java
    - Deleted NonTerminal.java due to redundancy

--- fall18-artificial-intelligence/CYKParser.java
+++ fall18-artificial-intelligence/CYKParser.java
@@ -26,7 +26,7 @@ public class CYKParser {
 			for (NonTerm POS : NonTerm.values()) {
 				for (String[] rule : POS.rules) {
 					if (rule.length == 2 && rule[0].equals(word))
-						this.chart[chart_indices.indexOf(POS.symbol)][i][i] = new Tree(POS, i, i, word, null, null, Double.parseDouble(rule[1]));
+						this.chart[POS.ordinal()][i][i] = new Tree(POS, i, i, word, null, null, Double.parseDouble(rule[1]));
 				}
 			}
 		}
@@ -35,12 +35,22 @@ public class CYKParser {
 			for (int i = 0; i < (N + 1 - len); i++) {
 				int j = i + len - 1;
 				for (NonTerm M : NonTerm.values()) {
-					if (M.rules[0].length == 3)
+					if (M.rules[0].length == 3) {
 						this.chart[M.ordinal()][i][j] = new Tree(M, i, j, null, null, null, 0.0);
+
+						for (int k = i; k < (j - 1); k++) {
+							for (String[] rule : M.rules) {
+								double newProb = (this.chart[chart_indices.indexOf(rule[0])][i][k]).prob * (this.chart[chart_indices.indexOf(rule[1])][k + 1][j]).prob * Double.parseDouble(rule[2]);
+
+								if (newProb > (this.chart[chart_indices.indexOf(M.symbol)][i][j]).prob) {
+									(this.chart[chart_indices.indexOf(M.symbol)][i][j]).left = this.chart[chart_indices.indexOf(rule[0])][i][k];
+									(this.chart[chart_indices.indexOf(M.symbol)][i][j]).right = this.chart[chart_indices.indexOf(rule[1])][k + 1][j];
+									(this.chart[chart_indices.indexOf(M.symbol)][i][j]).prob = newProb;
+								}
+							}
+						}
+					}
 				}
-				/*for (int k = i; k < (j - 1); k++) {
-					
-				}*/
 			}
 		}
 		
--- fall18-artificial-intelligence/CallingClass.java
+++ fall18-artificial-intelligence/CallingClass.java
@@ -1,12 +1,44 @@
 package artificial_intelligence_fall18;
 
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 public class CallingClass {
 	public static void main(String args[]) {
-		
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a sentence to parse: ");
+
+		String sentence = input.nextLine();
+
+		CYKParser parser = new CYKParser(sentence);
+
+		Tree[][][] chart = parser.parse();
+
+
+	}
+
+	public static void printParse(Tree[][][] chart, String sentence, int indentation) {
+		//Map NonTerm elements to chart indices
+		ArrayList<String> chart_indices = new ArrayList<>();
+		for (NonTerm e : NonTerm.values()) {
+			chart_indices.add(e.symbol);
+		}
+
+		Tree parse = chart[chart_indices.indexOf("S")][0][sentence.split().length - 1];
+
+		recursivePrint(parse, 0);
+	}
+
+	public static void recursivePrint(Tree parse, int indentation) {
+		for (int i = 0; i < indentation; i++) {
+			System.out.print(" ");
+		}
+
+		if (parse.phrase.rules[0].length == 2)
+			System.out.println(parse.phrase.symbol + " " + parse.word);
+		else
+			System.out.println(parse.phrase.symbol);
+			recursivePrint(parse.left, indentation + 3);
+			recursivePrint(parse.right, indentation + 3);
 	}
 }
