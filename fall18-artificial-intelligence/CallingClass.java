2dae268c03416da6ddb85894a71686e19275945c
positivevaib <vaib.gadodia@gmail.com>
2018-09-18 20:26:26 -0400

    Update CYKParser.java and CallingClass.java
    - Changed the parse function of CYKParser.java to accomodate the changes in the NonTerm class.
    - Deleted the main function body of CallingClass.java as with the changes in NonTerm class, grammar construction is not required.
    - Deleted Terminal.java due to redundancy.

--- fall18-artificial-intelligence/CallingClass.java
+++ fall18-artificial-intelligence/CallingClass.java
@@ -7,45 +7,6 @@ import java.util.ArrayList;
 
 public class CallingClass {
 	public static void main(String args[]) {
-		boolean parse = false;
 		
-		try {
-			Scanner reader = new Scanner(new File("grammar.txt"));
-			
-			ArrayList<NonTerminal> grammar = new ArrayList<>();
-			while (reader.hasNext()) {
-				String symbol = reader.next();
-				reader.next();
-				String leftSubphrase = reader.next();
-				String rightSubphrase = reader.next();
-				String prob_string = reader.next();
-				double prob = Double.parseDouble(prob_string.substring(1, prob_string.length() - 1));
-				
-				NonTerminal phrase = new NonTerminal(symbol, leftSubphrase, rightSubphrase, null, prob);
-				grammar.add(phrase);
-			}
-			
-			reader = new Scanner(new File("lexicon.txt"));
-
-			while (reader.hasNext()) {
-				String symbol = reader.next();
-				reader.next();
-				String word = reader.next();
-				String prob_string = reader.next();
-				double prob = Double.parseDouble(prob_string.substring(1, prob_string.length() - 1));
-				
-				NonTerminal phrase = new NonTerminal(symbol, null, null, word, prob);
-				grammar.add(phrase);
-			}
-			
-			parse = true;
-		}
-		catch (FileNotFoundException e) {
-			System.out.println("Error");
-		}
-		
-		if (parse) {
-			
-		}
 	}
 }
