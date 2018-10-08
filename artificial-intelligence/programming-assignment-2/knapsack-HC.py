a3acc745f9d26cd676ec173bbd56dc7b4a825e7a
positivevaib <vaib.gadodia@gmail.com>
2018-10-08 15:12:18 -0400

    Add comments and delete redundant functions

--- artificial-intelligence/programming-assignment-2/knapsack-HC.py
+++ artificial-intelligence/programming-assignment-2/knapsack-HC.py
@@ -1,149 +1,164 @@
+#Import packages
 import random
 import copy
-import pdb
-#pdb.set_trace()
+
+#Define Object class to reflect given objects
 class Object:
+    #Define variables
     name = ''
     index = 0
     weight = 0
     value = 0
 
+    #Define constructor
     def __init__(self, name, index, weight, value):
         self.name = name
         self.index = index
         self.weight = weight
         self.value = value
 
+#Define State class to reflect state spaces
 class State:
-    objects = []
-
+    #Define variables
+    objects = {}
     weight = 0
     value = 0
 
+    #Define constructor
     def __init__(self, objects):
         self.objects = objects
 
-    def add_object(self, object):
-        self.objects.append(object)
+    #Define function to add objects
+    def add(self, object):
+        self.objects[str(object.index)] = object
         self.weight += object.weight
         self.value += object.value
 
-    def add_object_index(self, object, index):
-        self.objects.insert(index, object)
-        self.weight += object.weight
-        self.value += object.value
+    #Define function to remove objects
+    def remove(self, object):
+        del self.objects[str(object.index)]
+        self.weight -= object.weight
+        self.value -= object.value
 
-    def remove_last_object(self):
-        self.weight -= self.objects[-1].weight
-        self.value -= self.objects[-1].value
-        self.objects = self.objects[:-1]
-
-    def remove_object(self, index):
-        self.weight -= self.objects[index].weight
-        self.value -= self.objects[index].value
-        self.objects.pop(index)
-
+#Define function to return random initial states
 def random_restart(object_dict):
-    state = State([])
+    state = State({})
     for object in object_dict.values():
         choice = random.choice([object, None])
         if choice:
-            state.add_object(choice)
+            state.add(choice)
 
     return state
 
+#Define function to compute and return state error
 def error(state, T, M):
     return max(state.weight - M, 0) + max(T - state.value, 0)
 
-def HCS(total_restarts, object_dict, T, M):
+#Define function to search using Hill Climbing Algorithm with random restarts and return all best states found
+def HCS(restarts, object_dict, T, M):
     best_states = []
-    for i in range(total_restarts):
+
+    #Search for all random restarts
+    for i in range(restarts):
+        #Initialize state and error
         state = random_restart(object_dict)
         state_error = error(state, T, M)
 
-
-        print('initial state')
-        for obj in state.objects:
-            print(obj.name, end = '  ')
-        print()
-
-        
+        #Search until no optimal neighbor states to move to
         while True:
+            #Initialize optimal neighbor state
             neighbor_state = None
             neighbor_error = state_error
 
-            index = 0
-            for object in state.objects:
-                state.remove_object(index)
+            #Delete objects
+            for object in state.objects.values():
+                state.remove(object)
+                #Update neighbor_state if error improves
                 if error(state, T, M) < neighbor_error:
                     neighbor_state = copy.deepcopy(state)
                     neighbor_error = error(state, T, M)
-                state.add_object_index(object, index)
-                
-                index += 1
+                #Revert to original state for next deletion
+                state.add(object)
 
+            #Add and swap objects
             for new_object in object_dict.values():
+                #Initialize sentinel value
                 change = True
-                for object in state.objects:
+
+                #Continue to next iteration if new_objects already in state
+                for object in state.objects.values():
                     if new_object.name == object.name:
+                        #Update sentinel value
                         change = False
                         break
+                
+                #If new_object not in state, add and swap objects
                 if change:
-                    state.add_object(new_object)
+                    #Add new_object
+                    state.add(new_object)
+                    #Update neighbor state if error improves
                     if error(state, T, M) < neighbor_error:
                         neighbor_state = copy.deepcopy(state)
                         neighbor_error = error(state, T, M)
-                    state.remove_last_object()
+                    #Revert to original state for swaps and next insertion
+                    state.remove(new_object)
 
-                    for index in range(len(state.objects)):
-                        object = state.objects[index]
-                       
-                        state.remove_object(index)
-                        state.add_object(new_object)
-                        
+                    #Swap objects with new_object
+                    for object in state.objects.values():
+                        state.remove(object)
+                        state.add(new_object)
+                        #Update neighbor state if error improves
                         if error(state, T, M) < neighbor_error:
                             neighbor_state = copy.deepcopy(state)
                             neighbor_error = error(state, T, M)
+                        #Revert to original state for further swaps
+                        state.remove(new_object)
+                        state.add(object)
 
-                        state.remove_last_object()
-                        state.add_object_index(object, index)
-
-            if not neighbor_state:
-                break
-            else:
+            #Update state and state_error for next iteration if an optimal neighbor state exists
+            if neighbor_state:
                 state = copy.deepcopy(neighbor_state)
                 state_error = neighbor_error
+            else:
+                break
+        
+        #Add best state found to best_states
+        best_states.append(state)
 
-        if state:
-            best_states.append(state)
-
+    #Return best states found for all restarts
     return best_states
 
+#Define main function
 def main():
+    #Read input and store necessary information
     object_dict = {}
-    file = open('input1.txt', 'r')
+
+    file = open('input2.txt', 'r')
     i = 1
     for line in file:
         line = line.strip().split()
+        #Assign target value and max weight to T and M respectively
         if len(line) == 2:
             T = int(float(line[0]))
             M = int(float(line[1]))
+        #Add objects to object_dict
         else:
             name = line[0]
             value = int(float(line[1]))
             weight = int(float(line[2]))
 
             object_dict[str(i)] = Object(name, i, weight, value)
-        i += 1
+            i += 1
     
     total_restarts = 10
-
+    #Search state space using HCS
     best_states = HCS(total_restarts, object_dict, T, M)
 
+    #Print best states
     for state in best_states:
-        print('[', end = '  ')
-        for object in state.objects:
-            print(object.name, end = '  ')
-        print(']')
+        print('Solution:', end = ' ')
+        for object in state.objects.values():
+            print(object.name, end = ' ')
+        print()
 
 main()
--- artificial-intelligence/programming-assignment-2/knapsack-ID.py
+++ artificial-intelligence/programming-assignment-2/knapsack-ID.py
@@ -1,64 +1,92 @@
+#Define Object class to reflect given objects
 class Object:
+    #Define variables
     name = ''
     index = 0
     weight = 0
     value = 0
 
+    #Define constructor
     def __init__(self, name, index, weight, value):
         self.name = name
         self.index = index
         self.weight = weight
         self.value = value
 
+#Define State class to reflect state spaces
 class State:
+    #Define variables
     objects = []
-
     weight = 0
     value = 0
 
+    #Define constructor
     def __init__(self, objects):
         self.objects = objects
 
-    def add_object(self, object):
+    #Define function to add objects
+    def add(self, object):
         self.objects.append(object)
         self.weight += object.weight
         self.value += object.value
 
-    def remove_last_object(self):
+    #Define function to remove objects
+    def remove(self):
         self.weight -= self.objects[-1].weight
         self.value -= self.objects[-1].value
         self.objects = self.objects[:-1]
 
-def IDS(depth, state):
-    object_dict = {'1': Object('A', 1, 8, 10), '2': Object('B', 2, 4, 8), '3': Object('C', 3, 3, 7), '4': Object('D', 4, 3, 6), '5': Object('E', 5, 1, 4)}
-
+#Define function to search using Iterative Deeping Algorithm and return the solution if it exists
+def IDS(state, depth, object_dict, T, M):
+    #Return state if it is a solution
     if depth == 0:
-        if state.value >= 20 and state.weight <= 10:
+        if state.value >= T and state.weight <= M:
             return state
         else:
             return None
+    #Use recursion to search deeper and broader
     else:
-        for i in range(state.objects[-1].index + 1, 6):
-            state.add_object(object_dict[str(i)])
-            found = IDS(depth - 1, state)
-            if found:
-                return found
-            state.remove_last_object()
+        for i in range(state.objects[-1].index + 1, len(object_dict) + 1):
+            state.add(object_dict[str(i)])
+            state_found = IDS(state, depth - 1, object_dict, T, M)
+            if state_found:
+                return state_found
+            state.remove()
         return None
 
+#Define main function
 def main():
-    object_dict = {'1': Object('A', 1, 8, 10), '2': Object('B', 2, 4, 8), '3': Object('C', 3, 3, 7), '4': Object('D', 4, 3, 6), '5': Object('E', 5, 1, 4)}
+    #Read input and store necessary information
+    object_dict = {}
 
+    file = open('input2.txt', 'r')
+    i = 1
+    for line in file:
+        line = line.strip().split()
+        #Assign target value and max weight to T and M respectively
+        if len(line) == 2:
+            T = int(float(line[0]))
+            M = int(float(line[1]))
+        #Add objects to object_dict
+        else:
+            name = line[0]
+            value = int(float(line[1]))
+            weight = int(float(line[2]))
+
+            object_dict[str(i)] = Object(name, i, weight, value)
+            i += 1
+
+    #Search state space using IDS and print solution
     for i in range(1, len(object_dict) + 1):
-        solution = IDS(i, State([Object('zero', 0, 0, 0)]))
+        solution = IDS(State([Object('zero', 0, 0, 0)]), i, object_dict, T, M)
         if solution:
-            print('[', end = "")
+            print('Solution:', end = ' ')
             for i in range(1, len(solution.objects)):
-                if i == len(solution.objects) - 1:
-                    print(solution.objects[i].name, end = "")
-                else:
-                    print(solution.objects[i].name, end = ', ')
-            print(']')
+                print(solution.objects[i].name, end = ' ')
+            print()
             break
+
+    if not solution:
+        print('No solution')
 
 main()
