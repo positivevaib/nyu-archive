2ff91456967da6c2f2d50cb38435710ce370a30b
positivevaib <vaib.gadodia@gmail.com>
2018-10-19 13:00:44 -0400

    Add code to find all nodes in a loop

--- basic-algorithms/programming-assignment-4/Solution.java
+++ basic-algorithms/programming-assignment-4/Solution.java
@@ -1,9 +1,3 @@
-
-/* To do:
-          Add all rooms in a loop instead of just the room with the back edge
-*/
-
-
 import java.io.*;
 import java.util.*;
 
@@ -11,7 +5,8 @@ public class Solution {
     static Graph G;
     static char[] colorList;
     static int[] parentList;
-    static ArrayList<Integer> backEdges = new ArrayList<>();
+    static int loopStart;
+    static int loopEnd;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -27,50 +22,79 @@ public class Solution {
             Solution.G.addEdge(from, to);
         }
 
+        for (int i = 0; i < G.edgeList.length; i++) {
+            System.out.print("\n"+ (i + 1) +" --> ");
+            Edge edge = G.edgeList[i];
+            if (edge != null) {
+            while (edge.next != null) {
+                System.out.print(edge.to + " ");
+                edge = edge.next;
+            }
+            System.out.print(edge.to + " ");         }
+        }
+
         Solution.DFS();
 
-        if (Solution.backEdges.size() == 0)
+        if (Solution.loopStart == -1)
             System.out.println(0);
         else {
             System.out.println(1);
             
-            for (int i = 0; i < Solution.backEdges.size(); i++)
-                System.out.print(Solution.backEdges.get(i) + " ");
+            ArrayList<Integer> loopVertices = new ArrayList<>();
+            System.out.println(loopStart + " " + loopEnd);
+            while (Solution.loopEnd != Solution.loopStart) {
+                loopVertices.add(Solution.loopEnd);
+                System.out.println(parentList[loopEnd - 1]);
+                Solution.loopEnd = Solution.parentList[Solution.loopEnd - 1];
+            }
+            loopVertices.add(Solution.loopEnd);
+
+            Collections.sort(loopVertices);
+
+            for (int i = 0; i < loopVertices.size(); i++)
+                System.out.print(loopVertices.get(i) + " ");
         }
     }
 
     static void DFS() {
         Solution.colorList = new char[Solution.G.edgeList.length];
         Solution.parentList = new int[Solution.G.edgeList.length];
+
+        Solution.loopStart = -1;
+        Solution.loopEnd = -1;
+
         for (int i = 0; i < Solution.G.edgeList.length; i++) {
             Solution.colorList[i] = 'w';
             Solution.parentList[i] = 0;
         }
 
+        boolean recurse = true;
         for (int i = 0; i < Solution.G.edgeList.length; i++) {
-            if (Solution.colorList[i] == 'w')
-                Solution.recDFS(i + 1);
+            if (Solution.colorList[i] == 'w' && recurse)
+                recurse = Solution.recDFS(i + 1);
         }
     }
 
-    static void recDFS(int vertex) {
+    static boolean recDFS(int vertex) {
         Solution.colorList[vertex - 1] = 'g';
-
+        boolean recurse = true;
         Edge edge = Solution.G.edgeList[vertex - 1];
         while (edge != null) {
             if (Solution.colorList[edge.to - 1] == 'w') {
                 Solution.parentList[edge.to - 1] = vertex;
-                Solution.recDFS(edge.to);
+                recurse = Solution.recDFS(edge.to);
             }
             else if (Solution.colorList[edge.to - 1] == 'g') {
-                if (!Solution.backEdges.contains(vertex))
-                    Solution.backEdges.add(vertex);
-            }
+                Solution.loopStart = edge.to;
+                Solution.loopEnd = vertex;
 
+                return false;
+            }
             edge = edge.next;
         }
 
         Solution.colorList[vertex - 1] = 'b';
+        return recurse;
     }
 }
 
