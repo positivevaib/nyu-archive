3cd6026d76572d8f210f0413420dd9a885b05676
Vaib Gadodia <positivevaib@users.noreply.github.com>
2020-04-22 16:29:34 -0400

    Add new modes

--- grad/high-performance-machine-learning/lab4/lab4.py
+++ grad/high-performance-machine-learning/lab4/lab4.py
@@ -28,16 +28,16 @@ def main(args):
                                             train=True,
                                             transform=transformations,
                                             download=True)
-    trainloader = torch.utils.data.DataLoader(trainset,
-                                              batch_size=(torch.cuda.device_count() *args.batch_size),
-                                              shuffle=True,
-                                              num_workers=2)
+    trainloader = torch.utils.data.DataLoader(
+        trainset,
+        batch_size=(torch.cuda.device_count() * args.batch_size),
+        shuffle=True,
+        num_workers=2)
 
     # setup neural net, training loss function and optimization algorithm
-    device = torch.device(
-        'cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-    net = resnet.ResNet18(False)
+    net = resnet.ResNet18(True)
 
     if torch.cuda.device_count() > 1:
         net = nn.DataParallel(net)
@@ -46,17 +46,26 @@ def main(args):
 
     criterion = nn.CrossEntropyLoss()
 
-    optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0005)
+    optimizer = optim.SGD(net.parameters(),
+                          lr=0.1,
+                          momentum=0.9,
+                          weight_decay=0.0005)
 
     # train
     print('training')
 
     for epoch in range(args.num_epochs):
-        train_time = 0  # Q1
-        running_time = 0  # Q2
-        running_start = time.perf_counter()  # Q2
+        run_time = 0  # Q1/Q2/Q3.1
+        running_loss = 0  # Q4
+        correct = 0  # Q4
+        total = 0  # Q4
+
+        if args.mode == 2:
+            run_start = time.perf_counter()  
+
         for i, data in enumerate(trainloader, 0):
-            train_start = time.perf_counter()  # Q1/Q2
+            if args.mode == 1 or args.mode == 3.1:
+                run_start = time.perf_counter()  
             inputs, labels = data[0].to(device), data[1].to(device)
 
             optimizer.zero_grad()
@@ -66,26 +75,55 @@ def main(args):
             loss.backward()
             optimizer.step()
 
-            train_time += time.perf_counter() - train_start  # Q1
-            running_time += time.perf_counter() - running_start  # Q2
+            if args.mode == 1 or args.mode == 2 or args.mode == 3.1:
+                run_time += time.perf_counter() - run_start 
 
-            print('[{}, {}]     '.format(epoch + 1, i + 1), end = '\r')
+            if args.mode == 4:
+                running_loss += loss.item()
+                _, predicted = torch.max(outputs.data, 1)
+                correct += (predicted == labels).sum().item()
+                total += labels.size(0) 
 
-            running_start = time.perf_counter()  # Q2
+            print('[{}, {}]             '.format(epoch + 1, i + 1), end='\r')
+
+            if args.mode == 2:
+                run_start = time.perf_counter()  
 
         if epoch != 0:
             if args.mode == 1:
-                print("train time per epoch: {} s.".format(train_time))
+                print("train time per epoch: {} s.".format(run_time))
             elif args.mode == 2:
-                print("train time per epoch: {} s.".format(running_time))
+                print("train time per epoch: {} s.".format(run_time))
+            elif args.mode == 3.1:
+                print("computation time per epoch {} s.".format(
+                    args.baseline / torch.cuda.device_count()))
+                print("communication time per epoch {} s.".format(
+                    run_time - (args.baseline / torch.cuda.device_count())))
+            elif args.mode == 4 and epoch == 4:
+                print("5th epoch - avg.train loss: {}, train acc.: {}%".format(
+                    running_loss / len(trainloader), correct / total * 100))
 
-    print('finished training')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str, help='data path')
-    parser.add_argument('--batch_size', type=int, default=32, help='training batch size per gpu', metavar='')
-    parser.add_argument('--mode', type=int, default=1, help='type of output specific to coding exercises - 1, 2, 3, 4', metavar='')
+    parser.add_argument('--batch_size',
+                        type=int,
+                        default=32,
+                        help='training batch size per gpu',
+                        metavar='')
+    parser.add_argument(
+        '--baseline',
+        type=float,
+        default=None,
+        help='training time baseline as measured with 1 GPU setup',
+        metavar='')
+    parser.add_argument('--mode',
+                        type=float,
+                        choices={1, 2, 3.1, 3.2, 4},
+                        default=1,
+                        help='type of output specific to coding exercises',
+                        metavar='')
     parser.add_argument('--num_epochs',
                         type=int,
                         default=2,
@@ -93,4 +131,12 @@ if __name__ == '__main__':
                         metavar='')
 
     args = parser.parse_args()
+
+    if args.mode == 3.1 and not args.baseline:
+        print('--mode 3.1 requires --baseline')
+        quit()
+    elif args.mode == 4 and not args.num_epochs >= 5:
+        print('--mode 4 requires at least 5 epochs')
+        quit()
+
     main(args)
